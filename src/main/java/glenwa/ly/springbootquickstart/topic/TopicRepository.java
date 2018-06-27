d7a769f9753a1571c7e29c6ddb50f61c395003ad
nwalgampaya <nalinw@gmail.com>
2018-06-27 16:07:55 +1000

    Adding the Spring Boot code to connect to a h2 database

---
+++ src/main/java/glenwa/ly/springbootquickstart/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package glenwa.ly.springbootquickstart.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+	
+}
